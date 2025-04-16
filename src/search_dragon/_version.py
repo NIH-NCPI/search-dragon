3a47d67b6ee39ef832eab62fc9f5d38a49d069ac
brendagutman <brenda.gutman@gmail.com>
2025-04-16 15:26:24 -0400

    Version

--- src/search_dragon/_version.py
+++ src/search_dragon/_version.py
@@ -1,9 +1,9 @@
-__version__ = "1.0.3"
+__version__ = "1.0.2"
 
 '''
 Change log
 1.0.0 - Initial creation
 1.0.1 - Add OLS api
 1.0.2 - Add UMLS api
-1.0.3 - Allow "/" codes. Account for missing ontology_prefix.
+
 '''
