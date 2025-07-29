71505966babee3fe0a3a9813df4e095e64c0edc6
Eric Torstenson <eric.s.torstenson@vumc.org>
2025-07-29 16:33:13 -0500

    Generic interface for building a logger. The interface can accept a stream handler that is already defined or instantiates one if necessary.

--- src/search_dragon/__init__.py
+++ src/search_dragon/__init__.py
@@ -1,17 +1,31 @@
 import logging
+from rich.logging import RichHandler
 
-LOGGING_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
+LOGGING_FORMAT = "%(message)s"
 
-# Create a logger
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
+_logger = None
+def logger(logid="", loglevel=logging.INFO, logformat=LOGGING_FORMAT, console_handler=None, filename=None):
+    """Establish a singleton logger that can be reused by multiple components. 
+    """
+    global _logger
+    if _logger is None:
+        _logger = logging.getLogger(logid)
 
-if not logger.handlers:
-    # Create a console handler for logging to the console
-    console_handler = logging.StreamHandler()
-    console_handler.setLevel(logging.DEBUG)
-    console_handler.setFormatter(logging.Formatter(LOGGING_FORMAT))
+        if filename is not None:
+            logging.basicConfig(filename=filename, encoding='utf-8', level=loglevel)
 
-    # Add handlers to the logger
-    logger.addHandler(console_handler)
+        _logger.setLevel(loglevel)
 
+        # Create a console handler for logging to the console
+        if not _logger.handlers:
+            if filename is None and console_handler is None:
+                console_handler = logging.StreamHandler()
+                
+            if console_handler:
+                console_handler.setLevel(loglevel)
+                console_handler.setFormatter(logging.Formatter(logformat))
+
+                # Add handlers to the logger
+                _logger.addHandler(console_handler)
+    return _logger
+        
