1a796a9302a2cd0da6987958f299f35718174112
Eric Torstenson <eric.s.torstenson@vumc.org>
2025-07-31 13:08:57 -0500

    Throw an exception if there is a problem getting the csv content

--- src/search_dragon/support/__init__.py
+++ src/search_dragon/support/__init__.py
@@ -1,4 +1,5 @@
-import importlib_resources
+from importlib import resources as importlib_resources
+from search_dragon import logger
 
 from csv import DictReader 
 
@@ -9,13 +10,16 @@ _support_details = (
 def ftd_ontology_lookup(csv_path=None):
     if csv_path is None:
         csv_path = _support_details / "ftd_ontology_lookup.csv"
-    
-    with open(csv_path, 'rt') as infile:
-        onto_data = {}
 
-        lkup = DictReader(infile, delimiter=',', quotechar='"')
-        for row in lkup:
-            onto_data[row['curie']] = row['system']
+    try:    
+        with open(csv_path, 'rt') as infile:
+            onto_data = {}
+
+            lkup = DictReader(infile, delimiter=',', quotechar='"')
+            for row in lkup:
+                onto_data[row['curie']] = row['system']
+    except Exception as e:
+        logger().error(f"An error was encountered when loading FTD Ontological Lookup data: {e}")
 
     return onto_data
 
