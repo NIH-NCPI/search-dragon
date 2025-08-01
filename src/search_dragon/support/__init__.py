e4da71d5a9b146cfcc98ac7d20540a994f6105f9
Eric Torstenson <eric.s.torstenson@vumc.org>
2025-08-01 17:26:05 -0500

    Using manifest to wedge the csv into the package

--- src/search_dragon/support/__init__.py
+++ src/search_dragon/support/__init__.py
@@ -1,11 +1,11 @@
 from importlib import resources as importlib_resources
 from search_dragon import logger
-
 from csv import DictReader 
 
 _support_details = importlib_resources.files("search_dragon") / "support"
 
 def ftd_ontology_lookup(csv_path=None):
+    """Return the lookup as a dictionary: curie=>system"""
     if csv_path is None:
         csv_path = _support_details / "ftd_ontology_lookup.csv"
 
