c68461c1a865663082ce4cce4648a01ae295086b
Eric Torstenson <eric.s.torstenson@vumc.org>
2025-08-01 16:54:22 -0500

    Updated approach in hopes it fixes the issue as an egg

--- src/search_dragon/support/__init__.py
+++ src/search_dragon/support/__init__.py
@@ -3,24 +3,21 @@ from search_dragon import logger
 
 from csv import DictReader 
 
-_support_details = (
-    importlib_resources.files("search_dragon") / "support"
-) 
+_support_details = importlib_resources.files("search_dragon") / "support"
 
 def ftd_ontology_lookup(csv_path=None):
     if csv_path is None:
         csv_path = _support_details / "ftd_ontology_lookup.csv"
 
-    try:    
-        with open(csv_path, 'rt') as infile:
-            onto_data = {}
-
-            lkup = DictReader(infile, delimiter=',', quotechar='"')
-            for row in lkup:
-                onto_data[row['curie']] = row['system']
+    onto_data = {}
+    try:
+        with importlib_resources.as_file(csv_path) as path:
+            with open(path, 'rt') as infile:
+                lkup = DictReader(infile, delimiter=',', quotechar='"')
+                for row in lkup:
+                    onto_data[row['curie']] = row['system']
     except Exception as e:
         logger().error(f"An error was encountered when loading FTD Ontological Lookup data: {e}")
 
     return onto_data
-
     
